3afa99624de8857a6485dc5b2d34a8371a0a8476
David Grant <david.grant@mixpanel.com>
2021-12-26 13:16:22 -0800

    9b

--- part9a/main.go
+++ part9a/main.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
+
+type xy struct {
+	y, x int
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -31,19 +36,30 @@ func main() {
 		}
 	}
 
+	// Part 1:
+
 	score := 0
-	lows := lowPoints(grid)
-	for _, l := range lows {
-		score += 1 + l
+	pts := lowPoints(grid)
+	for _, pt := range pts {
+		score += 1 + grid[pt.y][pt.x]
 	}
 	fmt.Println(score)
+
+	// Part 2:
+
+	var sizes []int
+
+	for _, pt := range pts {
+		seen := make(map[xy]struct{})
+		sizes = append(sizes, basinSize(grid, pt, seen))
+	}
+	sort.Ints(sizes)
+	l := len(sizes)
+	fmt.Println(sizes[l-1] * sizes[l-2] * sizes[l-3])
 }
 
-func lowPoints(grid [][]int) []int {
-	type xy struct {
-		y, x int
-	}
-	var lows []int
+func lowPoints(grid [][]int) []xy {
+	var lowPts []xy
 
 	for j, line := range grid {
 		for i, c := range line {
@@ -61,10 +77,30 @@ func lowPoints(grid [][]int) []int {
 			}
 
 			if best {
-				lows = append(lows, c)
+				lowPts = append(lowPts, xy{j, i})
 			}
 		}
 	}
 
-	return lows
+	return lowPts
+}
+
+func basinSize(grid [][]int, lowPoint xy, seen map[xy]struct{}) int {
+	val := grid[lowPoint.y][lowPoint.x]
+
+	for _, dydx := range []xy{{-1, 0}, {0, 1}, {1, 0}, {0, -1}} {
+		y := lowPoint.y + dydx.y
+		x := lowPoint.x + dydx.x
+		if y >= 0 && y < len(grid) && x >= 0 && x < len(grid[0]) {
+			pt := xy{y, x}
+			if _, ok := seen[pt]; !ok {
+				if grid[y][x] < 9 && grid[y][x] > val {
+					seen[pt] = struct{}{}
+					basinSize(grid, pt, seen)
+				}
+			}
+		}
+	}
+
+	return len(seen) + 1
 }
