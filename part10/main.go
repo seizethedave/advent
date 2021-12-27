ebd386be6ac4506c0494611de73f903722ab42b7
David Grant <david.grant@mixpanel.com>
2021-12-26 16:10:21 -0800

    10 part b

--- part10/main.go
+++ part10/main.go
@@ -4,21 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 
-	score := 0
+	part1score := 0
+
+	var part2scores []int
+	termScores := map[rune]int{')': 1, ']': 2, '}': 3, '>': 4}
+
 	for s.Scan() {
-		line := s.Text()
-		score += scoreLine(line)
+		s, expected := scoreLine(s.Text())
+		part1score += s
+
+		if s == 0 && len(expected) > 0 {
+			expScore := 0
+			for i := range expected {
+				expScore = expScore*5 + termScores[expected[len(expected)-1-i]]
+			}
+			part2scores = append(part2scores, expScore)
+		}
 	}
 
-	fmt.Println(score)
+	fmt.Println(part1score)
+
+	sort.Ints(part2scores)
+	fmt.Println(part2scores[len(part2scores)/2])
 }
 
-func scoreLine(line string) int {
+func scoreLine(line string) (int, []rune) {
 	entries := map[rune]rune{'[': ']', '<': '>', '{': '}', '(': ')'}
 	scores := map[rune]int{')': 3, ']': 57, '}': 1197, '>': 25137}
 	var expect []rune // A stack of closing runes to expect.
@@ -30,10 +46,10 @@ func scoreLine(line string) int {
 			if c == expect[len(expect)-1] {
 				expect = expect[:len(expect)-1]
 			} else {
-				return scores[c]
+				return scores[c], expect
 			}
 		}
 	}
 
-	return 0
+	return 0, expect
 }
