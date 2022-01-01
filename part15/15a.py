0b1c1d6275839adb7c95d90c50ab908ed320812c
David Grant <david.grant@mixpanel.com>
2022-01-01 12:38:44 -0800

    15b

--- part15/15a.py
+++ part15/15a.py
@@ -1,35 +1,46 @@
 import heapq
 import sys
 
-def neighbors(grid, pos):
+def neighbors(height, width, pos):
 	for dy, dx in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
 		y = pos[0] + dy
 		x = pos[1] + dx
-		if 0 <= y < len(grid) and 0 <= x < len(grid[0]):
+		if 0 <= y < height and 0 <= x < width:
 			yield (y, x)
 
-def scan(grid):
+def cost1(grid, pos):
+	return grid[pos[0]][pos[1]]
+
+def cost2(grid, pos):
+	y, yr = divmod(pos[0], len(grid))
+	x, xr = divmod(pos[1], len(grid[0]))
+	val = grid[yr][xr] + y + x
+	return ((val-1)%9) + 1
+
+def scan(grid, height, width, cost_func):
 	q = [(0, (0, 0))]
-	destination = (len(grid) - 1, len(grid[0]) - 1)
 	seen = set()
+	target = (height - 1, width - 1)
 
 	while q:
 		dist, pos = heapq.heappop(q)
 
-		if pos == destination:
+		if pos == target:
 			return dist
 		elif pos in seen:
 			continue
 
 		seen.add(pos)
 
-		for n in neighbors(grid, pos):
+		for n in neighbors(height, width, pos):
 			if n not in seen:
-				(ny, nx) = n
-				heapq.heappush(q, (dist + grid[ny][nx], n))
+				cost = dist + cost_func(grid, n)
+				heapq.heappush(q, (cost, n))
 
 if __name__ == "__main__":
 	grid = []
 	for line in sys.stdin:
 		grid.append([int(j) for j in line.rstrip()])
-	print(scan(grid))
+
+	print(scan(grid, len(grid), len(grid[0]), cost1))
+	print(scan(grid, len(grid) * 5, len(grid[0]) * 5, cost2))
