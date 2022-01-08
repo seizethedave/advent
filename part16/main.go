41b2f013da0224b8de2af7211c1535f62d11520a
David Grant <david.grant@mixpanel.com>
2022-01-08 09:09:59 -0800

    part2

--- part16/main.go
+++ part16/main.go
@@ -5,14 +5,91 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"math"
 	"os"
 
 	"github.com/pkg/errors"
 )
 
 const (
-	TypeLiteral int = 4
+	TypeSum = iota
+	TypeProduct
+	TypeMinimum
+	TypeMaximum
+	TypeLiteral
+	TypeGreaterThan
+	TypeLessThan
+	TypeEqualTo
 )
+
+func opSum(vals []int) int {
+	s := 0
+	for _, v := range vals {
+		s += v
+	}
+	return s
+}
+
+func opProduct(vals []int) int {
+	s := 1
+	for _, v := range vals {
+		s *= v
+	}
+	return s
+}
+
+func opMin(vals []int) int {
+	s := math.MaxInt
+	for _, v := range vals {
+		if v < s {
+			s = v
+		}
+	}
+	return s
+}
+
+func opMax(vals []int) int {
+	s := 0
+	for _, v := range vals {
+		if v > s {
+			s = v
+		}
+	}
+	return s
+}
+
+func opGt(vals []int) int {
+	if vals[0] > vals[1] {
+		return 1
+	}
+	return 0
+}
+
+func opLt(vals []int) int {
+	if vals[0] < vals[1] {
+		return 1
+	}
+	return 0
+}
+
+func opEq(vals []int) int {
+	if vals[0] == vals[1] {
+		return 1
+	}
+	return 0
+}
+
+type opFunc func([]int) int
+
+var ops = [...]opFunc{
+	TypeSum:         opSum,
+	TypeProduct:     opProduct,
+	TypeMinimum:     opMin,
+	TypeMaximum:     opMax,
+	TypeGreaterThan: opGt,
+	TypeLessThan:    opLt,
+	TypeEqualTo:     opEq,
+}
 
 type bitReader struct {
 	io.ByteReader
@@ -75,14 +152,10 @@ func readLiteral(r *bitReader) (int, error) {
 }
 
 func readPacket(r *bitReader) (int, error) {
-	versum := 0
-
-	packetVers, err := r.readBits(3)
+	_, err := r.readBits(3) // version
 	if err != nil {
 		return 0, err
 	}
-
-	versum += int(packetVers)
 
 	packetType, err := r.readBits(3)
 	if err != nil {
@@ -90,45 +163,55 @@ func readPacket(r *bitReader) (int, error) {
 	}
 
 	if packetType == TypeLiteral {
-		_, err := readLiteral(r)
+		// Eval and return literal.
+		lit, err := readLiteral(r)
 		if err != nil {
 			return 0, errors.Wrap(err, "reading literal")
 		}
-	} else {
-		// an operator.
-		lengthFlag, err := r.readBits(1)
+		return lit, nil
+	}
+
+	// Otherwise, a general operator.
+
+	lengthFlag, err := r.readBits(1)
+	if err != nil {
+		return 0, err
+	}
+
+	var subvalues []int
+
+	switch lengthFlag {
+	case 1:
+		subpackets, err := r.readBits(11)
 		if err != nil {
 			return 0, err
 		}
-		switch lengthFlag {
-		case 1:
-			subpackets, err := r.readBits(11)
-			if err != nil {
-				return 0, err
-			}
 
-			for i := 0; i < subpackets; i++ {
-				vers, err := readPacket(r)
-				if err != nil {
-					return 0, errors.Wrap(err, "read subpacket")
-				}
-				versum += vers
-			}
-		case 0:
-			length, err := r.readBits(15)
+		for i := 0; i < subpackets; i++ {
+			subval, err := readPacket(r)
 			if err != nil {
-				return 0, err
+				return 0, errors.Wrap(err, "read subpacket")
 			}
+			subvalues = append(subvalues, subval)
+		}
+	case 0:
+		length, err := r.readBits(15)
+		if err != nil {
+			return 0, err
+		}
 
-			for begin := r.offset; r.offset < begin+length; {
-				vers, err := readPacket(r)
-				if err != nil {
-					return 0, errors.Wrap(err, "read subpacket")
-				}
-				versum += vers
+		for begin := r.offset; r.offset < begin+length; {
+			subval, err := readPacket(r)
+			if err != nil {
+				return 0, errors.Wrap(err, "read subpacket")
 			}
+			subvalues = append(subvalues, subval)
 		}
 	}
 
-	return versum, nil
+	op := ops[packetType]
+	if op == nil {
+		return 0, fmt.Errorf("operator %X not found", packetType)
+	}
+	return op(subvalues), nil
 }
