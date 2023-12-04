aba670146614f8832c7634d7cff22e78bfbf0484
David Grant <seizethedave@gmail.com>
2023-12-04 09:03:06 -0800

    grid struct

--- 2023/day03/part1.rs
+++ 2023/day03/part1.rs
@@ -1,39 +1,66 @@
 use std::io;
 
-fn grid_get(grid: &Vec<char>, width: usize, y: usize, x: usize) -> (bool, char) {
-    let pos = (y * width + x) as usize;
-    match grid.get(pos) {
-        Some(c) => (true, *c),
-        None => (false, '!'),
-    }
+struct Grid<'a> {
+    g: &'a Vec<char>,
+    width: usize,
+    height: usize,
 }
 
-fn is_symbol(grid: &Vec<char>, width: usize, y: usize, x: usize) -> bool {
-    let (valid, c) = grid_get(grid, width, y, x);
-    valid && !c.is_ascii_digit() && c != '.'
-}
-
-fn near_symbol(grid: &Vec<char>, width: usize, y: usize, start: usize, end: usize) -> bool {
-    for i in start..=end {
-        if (y > 0 && is_symbol(grid, width, y - 1, i)) || is_symbol(grid, width, y + 1, i) {
-            return true;
+impl Grid<'_> {
+    fn get(&self, y: usize, x: usize) -> (bool, char) {
+        let pos = (y * self.width + x) as usize;
+        match self.g.get(pos) {
+            Some(c) => (true, *c),
+            None => (false, '!'),
         }
     }
 
-    // Check columns left and right of the number.
-
-    if start > 0 {
-        if (y > 0 && is_symbol(grid, width, y - 1, start - 1)) ||
-            is_symbol(grid, width, y, start - 1) ||
-            is_symbol(grid, width, y + 1, start - 1) {
-            return true;
-        }
+    fn is_symbol(&self, y: usize, x: usize) -> bool {
+        let (valid, c) = self.get(y, x);
+        valid && !c.is_ascii_digit() && c != '.'
     }
 
-    (y > 0 && is_symbol(grid, width, y - 1, end + 1)) ||
-        is_symbol(grid, width, y, end + 1) ||
-        is_symbol(grid, width, y + 1, end + 1)
+    fn near_symbol(&self, y: usize, start: usize, end: usize) -> bool {
+        for i in start..=end {
+            if (y > 0 && self.is_symbol(y - 1, i)) || self.is_symbol(y + 1, i) {
+                return true;
+            }
+        }
+    
+        // Check columns left and right of the number.
+    
+        if start > 0 {
+            if (y > 0 && self.is_symbol(y - 1, start - 1)) ||
+                self.is_symbol(y, start - 1) ||
+                self.is_symbol(y + 1, start - 1) {
+                return true;
+            }
+        }
+    
+        (y > 0 && self.is_symbol(y - 1, end + 1)) ||
+            self.is_symbol(y, end + 1) ||
+            self.is_symbol(y + 1, end + 1)
+    }
+
+    fn terminate_number(&self, y: usize, start: usize, end: usize) -> i32 {
+        if !self.near_symbol(y, start, end) {
+            return 0;
+        }
+
+        let mut tally: i32 = 0;
+        for i in start..=end {
+            let (valid, c) = self.get(y, i);
+            if !valid {
+                panic!("terminate_number expected valid spots");
+            }
+            if let Some(cn) = c.to_digit(10) {
+                tally = tally * 10 + (cn as i32);
+            }
+        }
+        tally
+    }
 }
+
 
 fn main() {
     let mut grid: Vec<char> = Vec::new();
@@ -47,23 +74,13 @@ fn main() {
         grid.extend(l.chars());
     }
 
-    fn terminate_number(gr: &Vec<char>, width: usize, y: usize, start: usize, end: usize) -> i32 {
-        if !near_symbol(gr, width, y, start, end) {
-            return 0;
-        }
+    let gg = Grid {
+        g: &grid,
+        width: width,
+        height: height,
+    };
 
-        let mut tally: i32 = 0;
-        for i in start..=end {
-            let (valid, c) = grid_get(gr, width, y, i);
-            if !valid {
-                panic!("terminate_number expected valid spots");
-            }
-            if let Some(cn) = c.to_digit(10) {
-                tally = tally * 10 + (cn as i32);
-            }
-        }
-        tally
-    }
+    
 
     let mut score = 0;
 
@@ -71,9 +88,9 @@ fn main() {
     let mut start: usize = 0;
     let mut running = false;
 
-    for y in 0..height {
-        for x in 0..width {
-            let (valid, ch) = grid_get(&grid, width, y, x);
+    for y in 0..gg.height {
+        for x in 0..gg.width {
+            let (valid, ch) = gg.get(y, x);
             if !valid {
                 panic!("past grid bounds?")
             }
@@ -83,13 +100,13 @@ fn main() {
                     running = true;
                 }
             } else if running {
-                score += terminate_number(&grid, width, y, start, x-1);
+                score += gg.terminate_number(y, start, x-1);
                 running = false;
             }
         }
 
         if running {
-            score += terminate_number(&grid, width, y, start, width-1);
+            score += gg.terminate_number(y, start, width-1);
             running = false;
         }
     }
