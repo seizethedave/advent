12d9f1c4777b246d693ad6f8f6f96a4dee2df541
David Grant <seizethedave@gmail.com>
2023-12-06 20:05:15 -0800

    day06

---
+++ 2023/day06/day06.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+def count_solutions(time, distance):
+    ct = 0
+    for i in range(1, time):
+        if (time - i) * i > distance:
+            ct += 1
+    return ct
+
+if __name__ == "__main__":
+    tl, dl = list(sys.stdin)
+    tl = tl.strip()
+    dl = dl.strip()
+    times = [int(v) for v in tl.split()[1:]]
+    distances = [int(v) for v in dl.split()[1:]]
+    spec = zip(times, distances)
+    print(math.prod(count_solutions(t, d) for t, d in spec))
+
+    # Part 2: join up the numbers and re-run them.
+    t = int("".join(str(n) for n in times))
+    d = int("".join(str(n) for n in distances))
+    print(count_solutions(t, d))
+    
