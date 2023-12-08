843c70d2fe73fd4624b1dd59ef9c08b53b877daa
David Grant <seizethedave@gmail.com>
2023-12-08 15:42:18 -0800

    part 2

--- 2023/day08/day08.rs
+++ 2023/day08/day08.rs
@@ -1,6 +1,22 @@
 use std::io;
 use std::collections::HashMap;
 
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        a.abs()
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a.abs() / gcd(a, b) * b.abs()
+}
+
+fn lcm_multi(ns: &Vec<i64>) -> i64 {
+    ns.into_iter().fold(1, |acc, n| lcm(acc, *n))
+}
+
 fn main() {
     let mut line_iter = io::stdin().lines();
     let lr_line = match line_iter.next().unwrap() {
@@ -31,19 +47,43 @@ fn main() {
         nodes.insert(lhs.to_string(), (left_turn.to_string(), right_turn.to_string()));
     }
 
+    // Part 1:
     let mut pos = "AAA";
     const DEST: &str = "ZZZ";
 
     for (i, dir) in lr_line.chars().cycle().enumerate() {
-        let opts = nodes.get(pos).expect("node not found!");
         pos = {
-            let (l, r) = opts;
+            let (l, r) = nodes.get(pos).expect("node not found!");
             if dir == 'L' { l } else { r }
         };
-
         if pos == DEST {
             println!("{}", i + 1);
             break;
         }
     }
+
+    // Part 2.
+    // Instead of a single position, track ALL positions.
+    // |A| = |Z| and that means every A origin touches a Z destination once then
+    // loops again. Thus we need to find the length of each path from A to Z then
+    // find the LCM of all of these paths.
+    let positions: Vec<&String> = nodes.keys().filter(|k| k.ends_with('A')).collect();
+    let mut pathlens: Vec<i64> = Vec::new();
+    pathlens.reserve_exact(positions.len());
+
+    for p in positions.iter() {
+        let mut thispos = *p;
+        for (i, dir) in lr_line.chars().cycle().enumerate() {
+            thispos = {
+                let (l, r) = nodes.get(thispos).expect("node not found!");
+                if dir == 'L' { l } else { r }
+            };
+            if thispos.ends_with('Z') {
+                pathlens.push(i as i64 + 1);
+                break;
+            }
+        }
+    }
+
+    println!("{}", lcm_multi(&pathlens));
 }
