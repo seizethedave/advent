f4ce8d10cdfe8aaaf57976a9dc31ed45669d77d3
David Grant <seizethedave@gmail.com>
2024-01-17 13:51:59 -0800

    Re-include A* heuristic func now that things are working, because it is ~8% faster.

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -2,7 +2,6 @@ import sys
 import heapq
 
 MAX_FORWARD = 3
-
 DIR_CHAR = {
     (1, 0): "v",
     (0, 1): ">",
@@ -20,8 +19,12 @@ def next_moves(last_move, remaining):
             # Can't back up.
             pass
         else:
-            # Turning.
+            # Turning. By emitting this move we've used up one of the
+            # MAX_FORWARD movements.
             yield (dy, dx, MAX_FORWARD - 1)
+
+def dist(y1: int, x1: int, y2: int, x2: int) -> int:
+    return abs(y2 - y1) + abs(x2 - x1)
 
 def search(grid, startY, startX, endY, endX):
     height = len(grid)
@@ -32,10 +35,10 @@ def search(grid, startY, startX, endY, endX):
         return int(grid[yy][xx])
 
     visited = set()
-    fringe = [(0, (startY, startX), (0, 0), 0)]
+    fringe = [(0, 0, (startY, startX), (0, 0), 0)]
 
     while fringe:
-        (heat, (vy, vx), last_move, remaining) = heapq.heappop(fringe)
+        (_, heat, (vy, vx), last_move, remaining) = heapq.heappop(fringe)
         if vy == endY and vx == endX:
             return heat
         if (vy, vx, last_move, remaining) in visited:
@@ -46,13 +49,12 @@ def search(grid, startY, startX, endY, endX):
             ny = vy + dy
             nx = vx + dx
             try:
-                nheat = get(ny, nx)
+                nheat = heat + get(ny, nx)
             except IndexError:
                 continue
-            heapq.heappush(fringe, (heat + nheat, (ny, nx), (dy, dx), nrem))
+            h = nheat + dist(ny, nx, endY, endX)
+            heapq.heappush(fringe, (h, nheat, (ny, nx), (dy, dx), nrem))
 
 if __name__ == "__main__":
     grid = [line.rstrip() for line in sys.stdin]
-    height = len(grid)
-    width = len(grid[0])
-    print(search(grid, 0, 0, height - 1, width - 1))
+    print(search(grid, 0, 0, len(grid) - 1, len(grid[0]) - 1))
