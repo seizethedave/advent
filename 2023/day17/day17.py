2dae1343ea2f371a13bdfc003acd553de7db78e7
David Grant <seizethedave@gmail.com>
2024-01-17 14:30:09 -0800

    17.2

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -1,7 +1,6 @@
 import sys
 import heapq
 
-MAX_FORWARD = 3
 DIR_CHAR = {
     (1, 0): "v",
     (0, 1): ">",
@@ -9,24 +8,38 @@ DIR_CHAR = {
     (0, -1): "<",
 }
 
-def next_moves(last_move, remaining):
-    for m in DIR_CHAR.keys():
-        dy, dx = m
-        if m == last_move:
-            if remaining > 0:
-                yield (dy, dx, remaining - 1)
-        elif m == (-last_move[0], -last_move[1]):
-            # Can't back up.
-            pass
+def next_moves(last_move, y, x, endY, endX, remaining, forward_min_max):
+    min_forward, max_forward = forward_min_max
+    strides = max_forward - remaining
+    backward = tuple(-v for v in last_move)
+
+    def gen_moves():
+        for m in DIR_CHAR.keys():
+            dy, dx = m
+            if m == last_move:
+                if remaining > 0:
+                    yield (dy, dx, remaining - 1)
+            elif m == backward:
+                # Can't back up.
+                pass
+            elif strides >= min_forward:
+                # Turning. By emitting this move we've used up one of the
+                # MAX_FORWARD movements.
+                yield (dy, dx, max_forward - 1)
+
+    # Suppress moves that would reach the target square without meeting
+    # min_forward movements.
+    for (ddy, ddx, drem) in gen_moves():
+        if (y + ddy, x + ddx) == (endY, endX):
+            if max_forward - drem >= min_forward:
+                yield (ddy, ddx, drem)
         else:
-            # Turning. By emitting this move we've used up one of the
-            # MAX_FORWARD movements.
-            yield (dy, dx, MAX_FORWARD - 1)
+            yield (ddy, ddx, drem)
 
 def dist(y1: int, x1: int, y2: int, x2: int) -> int:
     return abs(y2 - y1) + abs(x2 - x1)
 
-def search(grid, startY, startX, endY, endX):
+def search(grid, startY, startX, endY, endX, forward_min_max):
     height = len(grid)
     width = len(grid[0])
     def get(yy, xx):
@@ -39,13 +52,15 @@ def search(grid, startY, startX, endY, endX):
 
     while fringe:
         (_, heat, (vy, vx), last_move, remaining) = heapq.heappop(fringe)
+
         if vy == endY and vx == endX:
             return heat
+
         if (vy, vx, last_move, remaining) in visited:
             continue
         visited.add((vy, vx, last_move, remaining))
 
-        for dy, dx, nrem in next_moves(last_move, remaining):
+        for dy, dx, nrem in next_moves(last_move, vy, vx, endY, endX, remaining, forward_min_max):
             ny = vy + dy
             nx = vx + dx
             try:
@@ -57,4 +72,7 @@ def search(grid, startY, startX, endY, endX):
 
 if __name__ == "__main__":
     grid = [line.rstrip() for line in sys.stdin]
-    print(search(grid, 0, 0, len(grid) - 1, len(grid[0]) - 1))
+    height = len(grid)
+    width = len(grid[0])
+    print(search(grid, 0, 0, height - 1, width - 1, (0, 3)))
+    print(search(grid, 0, 0, height - 1, width - 1, (4, 10)))
