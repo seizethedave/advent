18dc1805b63a8c02465e3d12b7d1833a1a50af06
David Grant <seizethedave@gmail.com>
2024-03-01 16:58:09 -0800

    Rather than reference counting, do a sort of module name interning so that they become cheaply pass-by-copy integer.

--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
 use std::collections::HashMap;
 use std::collections::VecDeque;
-use std::rc::Rc;
 
 /*
 We'll have a hashmap that is all known modules {"name" => mod_instance}
@@ -18,41 +17,60 @@ const MODULE_BUTTON: &str = "button";
 const MODULE_BROADCAST: &str = "broadcaster";
 const MODULE_END: &str = "rx";
 
+type ModuleRef = usize;
+
+struct RefStore {
+    entries: HashMap<String, ModuleRef>,
+}
+
+impl RefStore {
+    fn get(&mut self, val: &str) -> ModuleRef {
+        match self.entries.get(val) {
+            Some(r) => *r,
+            None => {
+                let next = self.entries.len();
+                self.entries.insert(String::from(val), next);
+                next
+            }
+        }
+    }
+}
+
 enum Behavior {
     Broadcast,
-    Conjunction(HashMap<Rc<str>, bool>),
+    Conjunction(HashMap<ModuleRef, bool>),
     FlipFlop(bool),
 }
 
 impl Behavior {
-    fn call<C>(&mut self, neighbors: &Vec<Rc<str>>, sender: Rc<str>, high_pulse: bool, mut callback: C)
-        where C: FnMut(&Rc<str>, bool)
+    fn call<C>(&mut self, neighbors: &Vec<ModuleRef>, sender: ModuleRef, high_pulse: bool, mut callback: C)
+        where C: FnMut(ModuleRef, bool)
     {
         match *self {
             Self::Broadcast => {
                 for n in neighbors {
-                    callback(n, high_pulse)
+                    callback(*n, high_pulse)
                 }
             },
             Self::Conjunction(ref mut h) => {
                 h.insert(sender, high_pulse);
                 let pulse = h.values().any(|v| !*v);
                 for n in neighbors {
-                    callback(n, pulse)
+                    callback(*n, pulse)
                 }
             },
             Self::FlipFlop(ref mut on) => {
                 if !high_pulse {
                     *on = !*on;
                     for n in neighbors {
-                        callback(n, *on);
+                        callback(*n, *on);
                     }
                 }
             },
         }
     }
 
-    fn add_source(&mut self, source_module: Rc<str>) {
+    fn add_source(&mut self, source_module: ModuleRef) {
         match *self {
             Self::Broadcast => { },
             Self::Conjunction(ref mut h) => {
@@ -78,17 +96,17 @@ impl Behavior {
 }
 
 struct Module {
-    neighbors: Vec<Rc<str>>,
+    neighbors: Vec<ModuleRef>,
     behavior: Behavior,
 }
 
-fn read_input() -> HashMap<Rc<str>, Box<Module>> {
-    let mut modules: HashMap<Rc<str>, Box<Module>> = HashMap::new();
+fn read_input(ref_store: &mut RefStore) -> HashMap<ModuleRef, Box<Module>> {
+    let mut modules: HashMap<ModuleRef, Box<Module>> = HashMap::new();
 
     for line in io::stdin().lines() {
         let line = line.unwrap();
         let (lhs, rhs) = line.split_once(" -> ").unwrap();
-        let neighbors: Vec<Rc<str>> = rhs.split(", ").map(|s| Rc::from(s)).collect();
+        let neighbors: Vec<ModuleRef> = rhs.split(", ").map(|s| ref_store.get(s)).collect();
 
         let (mod_name, behavior) = if lhs.starts_with(PREFIX_FLIP_FLOP) {
             (&lhs[1..], Behavior::FlipFlop(false))
@@ -98,7 +116,7 @@ fn read_input() -> HashMap<Rc<str>, Box<Module>> {
             (lhs, Behavior::Broadcast)
         };
 
-        modules.insert(Rc::from(mod_name), Box::new(Module{
+        modules.insert(ref_store.get(mod_name), Box::new(Module{
             neighbors,
             behavior,
         }));
@@ -107,16 +125,16 @@ fn read_input() -> HashMap<Rc<str>, Box<Module>> {
     // Go back through and install source modules now that the modules map is
     // fully populated.
 
-    let mut connections: Vec<(Rc<str>, Rc<str>)> = Vec::new();
+    let mut connections: Vec<(ModuleRef, ModuleRef)> = Vec::new();
     for (k, v) in modules.iter() {
         for n in v.neighbors.iter() {
-            connections.push((k.clone(), n.clone()));
+            connections.push((*k, *n));
         }
     }
 
-    for (src, dest) in &connections {
-        if let Some(dest_module) = modules.get_mut(dest) {
-            dest_module.behavior.add_source(src.clone());
+    for (src, dest) in connections {
+        if let Some(dest_module) = modules.get_mut(&dest) {
+            dest_module.behavior.add_source(src);
         }
     }
 
@@ -124,18 +142,20 @@ fn read_input() -> HashMap<Rc<str>, Box<Module>> {
 }
 
 fn main() {
-    let mut mods = read_input();
-    let mut actions: VecDeque<(Rc<str>, Rc<str>, bool)> = VecDeque::new();
+    let mut ref_store = RefStore{entries: HashMap::new()};
+    let mut mods = read_input(&mut ref_store);
+    let mut actions: VecDeque<(ModuleRef, ModuleRef, bool)> = VecDeque::new();
 
-    let button_ref: Rc<str> = Rc::from(MODULE_BUTTON);
-    let broadcast_ref: Rc<str> = Rc::from(MODULE_BROADCAST);
+    let button_ref = ref_store.get(MODULE_BUTTON);
+    let broadcast_ref = ref_store.get(MODULE_BROADCAST);
+    let rx_ref = ref_store.get(MODULE_END);
 
     const PRESSES: i16 = 1000;
     let mut low_pulses: i64 = 0;
     let mut high_pulses: i64 = 0;
 
     for _i in 0..PRESSES {
-        actions.push_back((button_ref.clone(), broadcast_ref.clone(), false));
+        actions.push_back((button_ref, broadcast_ref, false));
 
         while let Some((sender, dest, high_pulse)) = actions.pop_front() {
             if high_pulse {
@@ -145,7 +165,7 @@ fn main() {
             }
             if let Some(c) = mods.get_mut(&dest) {
                 c.behavior.call(&c.neighbors, sender, high_pulse, |n, p| {
-                    actions.push_back((dest.clone(), n.clone(), p))
+                    actions.push_back((dest, n, p))
                 })
             }
         }
@@ -167,17 +187,17 @@ fn main() {
         if (ct % 1_000_000) == 0 {
             println!("{}", ct);
         }
-        actions.push_back((button_ref.clone(), broadcast_ref.clone(), false));
+        actions.push_back((button_ref, broadcast_ref, false));
 
         while let Some((sender, dest, high_pulse)) = actions.pop_front() {
-            if !high_pulse && *dest == *MODULE_END {
+            if !high_pulse && dest == rx_ref {
                 println!("{}", ct);
                 break 'outer;
             }
 
             if let Some(c) = mods.get_mut(&dest) {
                 c.behavior.call(&c.neighbors, sender, high_pulse, |n, p| {
-                    actions.push_back((dest.clone(), n.clone(), p))
+                    actions.push_back((dest, n, p))
                 })
             }
         }
